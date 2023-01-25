0bb2b4368bda391611620e104eb0d44fe096381b
Brandon T <98200133+btomecsko@users.noreply.github.com>
2023-01-25 12:41:03 -0600

    style edits to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,18 +1,11 @@
-import { Fragment, useState } from 'react';
+import { Fragment } from 'react';
 import { useNavigate } from "react-router-dom";
 //import LogoImg from "../assets/logo.png";
 
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 
 const Home = () => {
 
-  const [userData, setUserData] = useState([]);
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
-  const [company, setCompany] = useState();
-  const [team, setTeam] = useState();
-  const [teamName, setTeamName] = useState();
   const navigate = useNavigate();
 
 
@@ -22,15 +15,15 @@ const Home = () => {
         <h1>Welcome to the Pokemon Team Creator!</h1>
         <h3>Select an Option from Below...</h3>
       </div>
-        <Form className='login' >
+        <div className='home' >
         {/* <img src={LogoImg} alt="App Logo" className="logo" /> */}
-          <Button id="loginSubmit" type="submit" style={{textTransform: 'uppercase', fontSize: '12px', fontWeight: '700', textAlign: 'center', backgroundColor: 'inherit', border: '1px solid #1BA098', borderRadius: '100px', marginBottom: '5px'}}>
+          <Button id="teamBtn" type="submit" style={{textTransform: 'uppercase', fontSize: '12px', fontWeight: '700', textAlign: 'center', backgroundColor: '#222224', border: '1px solid #1BA098', borderRadius: '100px', marginBottom: '5px'}}>
             Create Team
           </Button>
-          <Button id="loginSubmit" type="submit" style={{textTransform: 'uppercase', fontSize: '12px', fontWeight: '700', textAlign: 'center', backgroundColor: 'inherit', border: '1px solid #1BA098', borderRadius: '100px', marginBottom: '5px'}}>
+          <Button id="pokedexBtn" type="submit" style={{textTransform: 'uppercase', fontSize: '12px', fontWeight: '700', textAlign: 'center', backgroundColor: '#222224', border: '1px solid #1BA098', borderRadius: '100px', marginBottom: '5px'}}>
             Pokedex
           </Button>
-        </Form>
+        </div>
         </Fragment>
       );
 
