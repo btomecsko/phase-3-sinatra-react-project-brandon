e1c893f172f2a2fad4cd2dad207ae49c45c9bb31
Brandon T <98200133+btomecsko@users.noreply.github.com>
2023-01-25 23:16:28 -0600

    search function works

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -5,12 +5,17 @@ import Form from 'react-bootstrap/Form';
 const Search = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState("");
 
+  function handleSubmit(e){
+    e.preventDefault();
+    onSearch(searchTerm);
+  }
+
   function handleChange(e) {
     setSearchTerm(e.target.value);
   }
 
   return (
-    <Form className="d-flex">
+    <Form className="d-flex" onSubmit={handleSubmit}>
         <Form.Control className="prompt" value={searchTerm} onChange={handleChange} placeholder="Search for Pokemon" />
         <i className="search icon" />
       </Form> 
