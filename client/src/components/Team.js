fab899f9c28829cbc9c659c270c71fe626a7e2ed
Brandon T <98200133+btomecsko@users.noreply.github.com>
2023-01-25 18:18:32 -0600

    setting up Teams page

--- client/src/components/Team.js
+++ client/src/components/Team.js
@@ -1,4 +1,56 @@
+import React, { Fragment, useState } from "react";
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 const Teams = () => {
+    const [teams, setTeams] = useState([]);
+
+    const [form, setForm] = useState({});
+
+    let handleChange = (e) => {
+        let name = e.target.name;
+        let value = e.target.value
+        setForm({
+            ...form,
+            [name]: value,
+        })
+    }
+
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        fetch("http://localhost:9292/teams", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(form),
+        })
+            .then((res) => res.json())
+            .then((data) => setTeams(data, ...teams));
+    };
+
+    return (
+        <Fragment>
+            <Card.Header className="fs-2 pokemonContainer"><b>Create Pokemon Team</b></Card.Header>
+            <Form onSubmit={handleSubmit} className="addPokemon" style={{ backgroundColor: '#3C4048' }}>
+                <Form.Group className="mb-3">
+                    <Form.Label><i>Enter Team Name</i></Form.Label>
+                    <Form.Control type="text" name="name" placeholder="Name" onChange={handleChange} />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                <Form.Label>Select Pokemon</Form.Label>
+                    <Form.Select type="text" name="pokemon_id" placeholder="Pokemon" onChange={handleChange}>
+                        <option>Disabled select</option>
+                    </Form.Select>
+                </Form.Group>
+                <Button style={{ backgroundColor: '#00ABB3' }} type="submit">
+                    Create Team
+                </Button>
+            </Form>
+        </Fragment>
+    );
 
 }
 
